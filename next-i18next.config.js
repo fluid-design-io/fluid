7b4f6d320e49b61857e56a9b2380d54a0359110f
Jiuzhen Pan <2216991777@qq.com>
2022-11-14 22:00:11 -0800

    refactor(config): removed legacy configs

--- next-i18next.config.js
+++ next-i18next.config.js
@@ -2,7 +2,6 @@ module.exports = {
   i18n: {
     defaultLocale: 'en',
     locales: ['en', 'zh'],
-    reloadOnPrerender: true,
   },
   /* serializeConfig: false, // because of the custom use i18next plugin
     use: [i18nextPlugin], */
