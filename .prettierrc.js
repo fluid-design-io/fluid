986ef0ceeee1cb31f5a5fc8a44ac747eeb717721
Jiuzhen Pan <2216991777@qq.com>
2023-02-03 20:53:37 -0800

    build: updated build deps

--- .prettierrc.js
+++ .prettierrc.js
@@ -4,36 +4,6 @@ module.exports = {
   jsxSingleQuote: true,
   tabWidth: 2,
   semi: true,
-  importOrder: [
-    // ext library & side effect imports
-    ['^@?\\w', '^\\u0000'],
-    // {s}css files
-    ['^.+\\.s?css$'],
-    // Lib and hooks
-    ['^@/lib', '^@/hooks'],
-    // static data
-    ['^@/data'],
-    // components
-    ['^@/components', '^@/container'],
-    // zustand store
-    ['^@/store'],
-    // Other imports
-    ['^@/'],
-    // relative paths up until 3 level
-    [
-      '^\\./?$',
-      '^\\.(?!/?$)',
-      '^\\.\\./?$',
-      '^\\.\\.(?!/?$)',
-      '^\\.\\./\\.\\./?$',
-      '^\\.\\./\\.\\.(?!/?$)',
-      '^\\.\\./\\.\\./\\.\\./?$',
-      '^\\.\\./\\.\\./\\.\\.(?!/?$)',
-    ],
-    ['^@/types'],
-    // other that didnt fit in
-    ['^'],
-  ],
   importOrderSeparation: true,
   importOrderSortSpecifiers: true
 };
