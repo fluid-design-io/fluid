2b1a394cc76cb2ee7b3e63daef3481ba88d9d4d0
Jiuzhen Pan <2216991777@qq.com>
2023-02-03 20:42:39 -0800

    refactor(i18): removed support for i18n, use web-based translation instead

--- next-i18next.config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  i18n: {
-    defaultLocale: 'en',
-    locales: ['en', 'zh'],
-  },
-  /* serializeConfig: false, // because of the custom use i18next plugin
-    use: [i18nextPlugin], */
-};
